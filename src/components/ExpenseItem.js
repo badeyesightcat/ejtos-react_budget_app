8c29b8e294930e1ed0538367b3432d9abb91a72f
Beom Mee <whitebigbabybird@gmail.com>
2023-06-24 18:22:47 +0900

    Add work for practice project

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,35 @@
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense,
+        });
+    };
+
+    return <tr>
+        <td>{props.name}</td>
+        <td>￡{props.cost}</td>
+        <td><button onClick={e => increaseAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDeleteExpense} /></td>
+    </tr>;
+};
+
+export default ExpenseItem;
